ec69edf61ddd4788e81293b31fcd10d1df67af6d
Gosudarev Danila <danidagosudarev@gmail.com>
2025-09-21 16:02:21 +0300

    finish statistics module

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 import { StatisticsService } from "./statistics.service";
+import { JwtAuth } from "src/auth/decorators/jwt-auth.decorator";
 
 @Controller("statistics")
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
+
+  @Get("main/:storeId")
+  @JwtAuth()
+  getMainStatistics(@Param("storeId") storeId: string) {
+    return this.statisticsService.getMainStatistics(storeId);
+  }
+
+  @Get("middle/:storeId")
+  @JwtAuth()
+  getMiddleStatistics(@Param("storeId") storeId: string) {
+    return this.statisticsService.getMiddleStatistics(storeId);
+  }
 }
